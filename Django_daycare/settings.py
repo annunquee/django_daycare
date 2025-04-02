6bf9fa589dba6c4b19fa8bf2740acdc8c042aa5d
queen <kafikoa77@gmail.com>
2025-04-02 11:40:39 +0100

    Altered Static files in setting

--- Django_daycare/settings.py
+++ Django_daycare/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "whitenoise.runserver_nostatic",  # Add this before 'django.contrib.staticfiles'
 
     # My Apps
     'projects',
